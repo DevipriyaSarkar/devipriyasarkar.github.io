59718fc66ee9de756586467c8e28b455ce8b8339
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2016-02-14 22:32:51 +0530

    Initial Commit

---
+++ js/contact_me.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    $("#contactFrom input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+            
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+
+            var link = "mailto:devipriyasarkar96@gmail.com"
+             + "?subject=" + escape("This is my subject")
+             + "&body=" + escape(message);
+
+            window.location.href = link;
+            return false;
+
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function() {
+    $('#success').html('');
+});
