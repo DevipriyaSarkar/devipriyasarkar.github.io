e9ef1d4822cc26149a513af7b7868dfba4d47ebc
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-01-05 17:59:50 +0530

    chore: change website page
    add nav bar, fab and top section

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+        $(".typed").typed({
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 30,
+            loop: true,
+            loopCount: false,
+            showCursor: true
+        });
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
