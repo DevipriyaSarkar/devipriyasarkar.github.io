501765d59d00e4149c48fa3c592bf8c9a513c6fe
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-01-09 01:29:03 +0530

    chore: add skills section, add project list animation

--- js/init.js
+++ js/init.js
@@ -12,5 +12,22 @@
             showCursor: true
         });
 
-    }); // end of document ready
-})(jQuery); // end of jQuery name space
+        var options = [
+            {
+                selector: '#projects', offset: 50, callback: function(el) {
+                    Materialize.showStaggeredList('#project-staggered-list');
+                }
+            },
+            {
+                selector: '#skills', offset: 50, callback: function (el) {
+                    $('.skill-container > div').each(function(){
+                        var width = $(this).data('width');
+                        $(this).animate({ width: width }, 1500);
+                    });
+                }
+            }
+        ];
+        Materialize.scrollFire(options);
+
+    });
+})(jQuery);
