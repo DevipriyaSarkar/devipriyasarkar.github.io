f666572248386a0ed66c3129a5c24eee70f1abef
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-03-31 16:25:59 +0530

    chore: add go to top FAB at the bottom

--- js/init.js
+++ js/init.js
@@ -15,19 +15,25 @@
         var options = [
             {
                 selector: '#projects', offset: 50, callback: function(el) {
-                    Materialize.showStaggeredList('#project-staggered-list');
-                }
+                Materialize.showStaggeredList('#project-staggered-list');
+            }
             },
             {
                 selector: '#skills', offset: 50, callback: function (el) {
-                    $('.skill-container > div').each(function(){
-                        var width = $(this).data('width');
-                        $(this).animate({ width: width }, 1500);
-                    });
-                }
+                $('.skill-container > div').each(function(){
+                    var width = $(this).data('width');
+                    $(this).animate({ width: width }, 1500);
+                });
+            }
             }
         ];
         Materialize.scrollFire(options);
 
     });
+
+    $("#go-to-top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
 })(jQuery);
