eab00d96fe44ae22a24e979c00e0729181d0f02d
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-12-14 00:08:17 +0530

    chore: redesign website look

--- js/init.js
+++ js/init.js
@@ -1,5 +1,9 @@
 (function($){
   $(function(){
+      $('.parallax').parallax();
+      $('.fixed-action-btn').floatingActionButton({
+          direction: 'left'
+      });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
