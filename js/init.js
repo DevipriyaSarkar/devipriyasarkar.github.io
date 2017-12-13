7b5f0b5be9051f9437b32bfcb2be56eff8a9603d
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2017-12-13 20:05:50 +0530

    chore: wipe it fresh clean

--- js/init.js
+++ js/init.js
@@ -1,39 +1,5 @@
 (function($){
-    $(function(){
+  $(function(){
 
-        $('.button-collapse').sideNav();
-        $('.parallax').parallax();
-        $('.collapsible').collapsible();
-        $(".typed").typed({
-            stringsElement: $('#typed-strings'),
-            typeSpeed: 30,
-            loop: true,
-            loopCount: false,
-            showCursor: true
-        });
-
-        var options = [
-            {
-                selector: '#projects', offset: 50, callback: function(el) {
-                Materialize.showStaggeredList('#project-staggered-list');
-            }
-            },
-            {
-                selector: '#skills', offset: 50, callback: function (el) {
-                $('.skill-container > div').each(function(){
-                    var width = $(this).data('width');
-                    $(this).animate({ width: width }, 1500);
-                });
-            }
-            }
-        ];
-        Materialize.scrollFire(options);
-
-    });
-
-    $("#go-to-top").click(function() {
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
-
-})(jQuery);
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
