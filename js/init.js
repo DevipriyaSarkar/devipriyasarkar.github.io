32455308965130e27fb1d7bbb2aeb1847e6dd70b
Devipriya Sarkar <devipriyasarkar96@gmail.com>
2018-01-06 22:34:20 +0530

    chore: add pre-loader screen

--- js/init.js
+++ js/init.js
@@ -1,5 +1,14 @@
 (function($){
   $(function(){
+
+      // fade out pre-loader on load after delayTime
+      $(window).load(function() {
+          var delayTime = 5000;
+          var preLoader = $('#pre_loader');
+          preLoader.delay(delayTime).fadeOut('slow');
+      });
+
+      // for the top header parallax effect
       $('.parallax').parallax();
 
   }); // end of document ready
